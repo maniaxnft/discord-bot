dea0dd75d667794c74394d27c11a4e51dd40d3e8
onuriltan <onur.iltan@gmail.com>
2022-04-14 09:33:27 +0300

    fix: logging

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,12 @@
 const wait = require("timers/promises").setTimeout;
 
 const sendErrorToLogChannel = async (bot, message, e) => {
-  if (bot && message && e?.message) {
+  if (bot && message) {
     const channel = await bot.channels?.cache?.get(
       process.env.DISCORD_BOT_INFO_CHANNEL_ID
     );
     if (channel) {
-      channel.send(`${message}: ${e.message}`);
+      channel.send(`${message}: ${e?.message}`);
     }
   }
 };
