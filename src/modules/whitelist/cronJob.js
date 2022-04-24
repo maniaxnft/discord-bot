8a3ee5f01ce8d1603417f4b6bba2f0ea3173ba91
onuriltan <onur.iltan@gmail.com>
2022-04-24 17:17:49 +0300

    fix: await and send error

--- src/modules/whitelist/cronJob.js
+++ src/modules/whitelist/cronJob.js
@@ -5,17 +5,21 @@ const { remainingWhitelistModel } = require("./models");
 const cronJob = (bot) => {
   //run every 10 to 20 mins
   cron.schedule("10-20 * * * *", async () => {
-    const whitelistCountChannel = await bot?.channels?.cache?.get(
-      process.env.DISCORD_WHITELIST_INFO_CHANNEL_ID
-    );
-    const remaining = remainingWhitelistModel.findOne();
-    if (remaining && whitelistCountChannel) {
-      whitelistCountChannel.setName(`Whitelist: ${remaining.count} / 1300`);
-    } else {
-      sendErrorToLogChannel(
-        bot,
-        "remainingWhitelistModel or whitelistCountChannel is undefined"
+    try {
+      const whitelistCountChannel = await bot?.channels?.cache?.get(
+        process.env.DISCORD_WHITELIST_INFO_CHANNEL_ID
       );
+      const remaining = await remainingWhitelistModel.findOne();
+      if (remaining && whitelistCountChannel) {
+        whitelistCountChannel.setName(`Whitelist: ${remaining.count} / 1300`);
+      } else {
+        sendErrorToLogChannel(
+          bot,
+          "remainingWhitelistModel or whitelistCountChannel is undefined"
+        );
+      }
+    } catch (e) {
+      sendErrorToLogChannel(bot, "Error on cron job", e);
     }
   });
 };
