4486719ff4c875992e0e73dc1543ef1fab342b4c
onuriltan <onur.iltan@gmail.com>
2022-04-26 15:28:03 +0300

    feat: delete whitelist if message removed

--- src/modules/whitelist/updateRemainingWhitelist.js
+++ src/modules/whitelist/updateRemainingWhitelist.js
@@ -4,6 +4,14 @@ const { remainingWhitelistModel } = require("./models");
 
 const updateRemainingWhitelist = (bot) => {
   bot.on("messageCreate", async (message) => {
+    await updateWhitelistStatus(message, 1);
+  });
+
+  bot.on("messageDelete", async (message) => {
+    await updateWhitelistStatus(message, 0);
+  });
+
+  const updateWhitelistStatus = async (message, type) => {
     try {
       let remainigModel = await remainingWhitelistModel.findOne();
       if (!remainigModel) {
@@ -12,13 +20,28 @@ const updateRemainingWhitelist = (bot) => {
       }
       let count = remainigModel.count;
       if (message.channelId === process.env.DISCORD_GOLD_CHANNEL_ID) {
-        count = count + 10;
+        if (type === 1) {
+          count += 10;
+        }
+        if (type === 0) {
+          count -= 10;
+        }
       }
       if (message.channelId === process.env.DISCORD_SILVER_CHANNEL_ID) {
-        count = count + 8;
+        if (type === 1) {
+          count += 8;
+        }
+        if (type === 0) {
+          count -= 8;
+        }
       }
       if (message.channelId === process.env.DISCORD_BRONZ_CHANNEL_ID) {
-        count = count + 6;
+        if (type === 1) {
+          count += 6;
+        }
+        if (type === 0) {
+          count -= 6;
+        }
       }
       await remainingWhitelistModel.findByIdAndUpdate(
         remainigModel._id.toString(),
@@ -31,7 +54,7 @@ const updateRemainingWhitelist = (bot) => {
         e
       );
     }
-  });
+  };
   cronJob(bot);
 };
 
