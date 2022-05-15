82dc53a078d7499bc188ba06a6ae22b2811a7ef1
onuriltan <onur.iltan@gmail.com>
2022-05-15 21:34:37 +0300

    Update serverStats.js

--- src/modules/serverStats.js
+++ src/modules/serverStats.js
@@ -5,45 +5,20 @@ const { sendErrorToLogChannel, wait } = require("../utils");
 const { remainingWhitelistModel } = require("./whitelist/models");
 
 const updateServerStats = async (bot) => {
-  let waitInMs = 0;
   // eslint-disable-next-line no-constant-condition
   while (true) {
     // https://stackoverflow.com/a/62792412/3756237
-    const shouldWaitWhitelist = await updateWhitelistCount(bot);
-    shouldWaitWhitelist
-      ? (waitInMs = waitInMs + 300000)
-      : (waitInMs = waitInMs + 0);
-    if (waitInMs >= 600000) {
-      await wait(waitInMs);
-      waitInMs = 0;
-    }
+    await updateWhitelistCount(bot);
+    await wait(300001);
 
-    const shouldWaitMemberCount = await updateMemberCount(bot);
-    shouldWaitMemberCount
-      ? (waitInMs = waitInMs + 300000)
-      : (waitInMs = waitInMs + 0);
-    if (waitInMs >= 600000) {
-      await wait(waitInMs);
-      waitInMs = 0;
-    }
+    await updateMemberCount(bot);
+    await wait(300001);
 
-    const shouldWaitOnlineCount = await updateOnlineCount(bot);
-    shouldWaitOnlineCount
-      ? (waitInMs = waitInMs + 300000)
-      : (waitInMs = waitInMs + 0);
-    if (waitInMs >= 600000) {
-      await wait(waitInMs);
-      waitInMs = 0;
-    }
+    await updateOnlineCount(bot);
+    await wait(300001);
 
-    const shouldWaitTwitterCount = await updateTwitterCount(bot);
-    shouldWaitTwitterCount
-      ? (waitInMs = waitInMs + 300000)
-      : (waitInMs = waitInMs + 0);
-    if (waitInMs >= 600000) {
-      await wait(waitInMs);
-      waitInMs = 0;
-    }
+    await updateTwitterCount(bot);
+    await wait(300001);
   }
 };
 
@@ -56,12 +31,9 @@ const updateWhitelistCount = async (bot) => {
     const whitelistCountChannelName = `Whitelist: ${remaining.count} / 1300`;
     if (remaining && whitelistCountChannel.name !== whitelistCountChannelName) {
       whitelistCountChannel.setName(whitelistCountChannelName);
-      return true;
     }
-    return false;
   } catch (e) {
     sendErrorToLogChannel(bot, "Error at updateWhitelistCount", e);
-    return false;
   }
 };
 
@@ -82,12 +54,9 @@ const updateMemberCount = async (bot) => {
       memberCountChannel.name !== memberCountChannelName
     ) {
       memberCountChannel.setName(memberCountChannelName);
-      return true;
     }
-    return false;
   } catch (e) {
     sendErrorToLogChannel(bot, "Error at updateOnlineCount", e);
-    return false;
   }
 };
 
@@ -109,12 +78,9 @@ const updateOnlineCount = async (bot) => {
       onlineUsersCountChannel.name !== onlineUsersCountChannelName
     ) {
       onlineUsersCountChannel.setName(onlineUsersCountChannelName);
-      return true;
     }
-    return false;
   } catch (e) {
     sendErrorToLogChannel(bot, "Error at updateOnlineCount", e);
-    return false;
   }
 };
 
@@ -132,12 +98,9 @@ const updateTwitterCount = async (bot) => {
       twitterFollowerCountChannel.name !== twitterFollowerCountChannelName
     ) {
       twitterFollowerCountChannel.setName(twitterFollowerCountChannelName);
-      return true;
     }
-    return false;
   } catch (e) {
     sendErrorToLogChannel(bot, "Error at updateTwitterCount", e);
-    return false;
   }
 };
 
